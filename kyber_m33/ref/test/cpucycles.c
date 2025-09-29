9e31118e78799b06100d5aa2364c9981ffa45be3
Arnesen, Halvor <halvor.arnesen@nordicsemi.no>
2025-09-29 14:37:02 +0200

    Makefile WIP, cpucycles.h/.c changed to m33 core

--- kyber_m33/ref/test/cpucycles.c
+++ kyber_m33/ref/test/cpucycles.c
@@ -1,23 +1,13 @@
 #include <stdint.h>
 #include "cpucycles.h"
 
-
-#ifdef defined(__arm__) || defined(_M_ARM)
-
 uint32_t cpucycles_overhead(void) {
-	uint32_t t0, t1, overhead = -1L;
-
-#elif defined(__x86_64__) || defined(_M_X64)
-
-uint64_t cpucycles_overhead(void) {
-  uint64_t t0, t1, overhead = -1LL;
-#endif
-  
+	uint64_t t0, t1, overhead = 1LL;
   unsigned int i;
 
   for(i=0;i<100000;i++) {
     t0 = cpucycles();
-    __asm__ volatile ("");
+    __asm__ volatile (""::: "memory");
     t1 = cpucycles();
     if(t1 - t0 < overhead)
       overhead = t1 - t0;
--- kyber_m33/ref/test/cpucycles.h
+++ kyber_m33/ref/test/cpucycles.h
@@ -4,6 +4,9 @@
 #include <stdint.h>
 
 
+DWT DWT_TYPE
+
+
 
 #ifdef defined(__x86_64__) || defined(_M_X64)
 #ifdef USE_RDPMC  /* Needs echo 2 > /sys/devices/cpu/rdpmc */
@@ -36,20 +39,30 @@ uint64_t cpucycles_overhead(void);
 #elif defined(__arm__) || defined(_M_ARM)
 
 #include "core_cm33.h"
-
-static inline uint32_t cpu_cycles(void) {
-	
-	
-	result = DWT->CYCCNT;
-	return result;
+;
+static inline void cpucycles_init(void)
+{
+  DCB->DEMCR |= DCB_DEMCR_TRCENA_Msk;
+  DWT->CYCCNT = 0;
+  DWT->CTRL |= DWT_CTRL_CYCCNTENA_Msk
 }
 
+static inline uint32_t cpucycles(void) {
 
-static inline uint32_t cpu_overhead(void)
+
+  uint32_t last;
+  uint32_t high;
+	uint32_t now = DWT->CYCCNT;
+  high += (now < last);
+  last = now;
+
+	return ((uint64_t)high << 32) | now; 
+}
 
 
 #else
 
-static inline uint64_t cpu_cycles(void) { return 0; }
+static inline uint64_t cpucycles(void) { return 0; }
 
 #endif
+
