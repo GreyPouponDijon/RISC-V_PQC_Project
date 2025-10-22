d7ee76ac07bc3f55aea7353f43c253acd5f7a525
Arnesen, Halvor <halvor.arnesen@nordicsemi.no>
2025-10-22 10:51:55 +0200

    Readded Kyber Reference Implementation, updated Makefile

---
+++ pqc_m33/kyber_ref/randombytes.c
@@ -0,0 +1,78 @@
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include "randombytes.h"
+
+#ifdef _WIN32
+#include <windows.h>
+#include <wincrypt.h>
+#else
+#include <fcntl.h>
+#include <errno.h>
+#ifdef __linux__
+#define _GNU_SOURCE
+#include <unistd.h>
+#include <sys/syscall.h>
+#elif __NetBSD__
+#include <sys/random.h>
+#else
+#include <unistd.h>
+#endif
+#endif
+
+#ifdef _WIN32
+void randombytes(uint8_t *out, size_t outlen) {
+  HCRYPTPROV ctx;
+  size_t len;
+
+  if(!CryptAcquireContext(&ctx, NULL, NULL, PROV_RSA_FULL, CRYPT_VERIFYCONTEXT))
+    abort();
+
+  while(outlen > 0) {
+    len = (outlen > 1048576) ? 1048576 : outlen;
+    if(!CryptGenRandom(ctx, len, (BYTE *)out))
+      abort();
+
+    out += len;
+    outlen -= len;
+  }
+
+  if(!CryptReleaseContext(ctx, 0))
+    abort();
+}
+#elif defined(__linux__) && defined(SYS_getrandom)
+void randombytes(uint8_t *out, size_t outlen) {
+  ssize_t ret;
+
+  while(outlen > 0) {
+    ret = syscall(SYS_getrandom, out, outlen, 0);
+    if(ret == -1 && errno == EINTR)
+      continue;
+    else if(ret == -1)
+      abort();
+
+    out += ret;
+    outlen -= ret;
+  }
+}
+#elif defined(__NetBSD__)
+void randombytes(uint8_t *out, size_t outlen) {
+  ssize_t ret;
+
+  while(outlen > 0) {
+    ret = getrandom(out, outlen, 0);
+    if(ret == -1 && errno == EINTR)
+      continue;
+    else if(ret == -1)
+      abort();
+
+    out += ret;
+    outlen -= ret;
+  }
+}
+#else
+void randombytes(uint8_t *out, size_t outlen) {
+  //printf("randombytes(%d, %d)\n", out, outlen);
+  while(outlen--) *out++ = 0x29;
+}
+#endif
---
+++ pqc_m33/kyber_ref/test/speed_print.c
@@ -0,0 +1,69 @@
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "cpucycles.h"
+#include "speed_print.h"
+
+static int cmp_uint64(const void *a, const void *b) {
+  if(*(uint64_t *)a < *(uint64_t *)b) return -1;
+  if(*(uint64_t *)a > *(uint64_t *)b) return 1;
+  return 0;
+}
+
+static uint64_t median(uint64_t *l, size_t llen) {
+  qsort(l,llen,sizeof(uint64_t),cmp_uint64);
+
+  if(llen%2) return l[llen/2];
+  else return (l[llen/2-1]+l[llen/2])/2;
+}
+
+static uint64_t average(uint64_t *t, size_t tlen) {
+  size_t i;
+  uint64_t acc=0;
+
+  for(i=0;i<tlen;i++)
+    acc += t[i];
+
+  return acc/tlen;
+}
+
+void print_results(const char *s, uint64_t *t, size_t tlen) {
+  size_t i;
+  static uint64_t overhead = -1;
+
+  if(tlen < 2) {
+    printf(stderr, "ERROR: Need a least two cycle counts!\n");
+    return;
+  }
+
+  if(overhead  == (uint64_t)-1)
+    overhead = cpucycles_overhead();
+
+  tlen--;
+  for(i=0;i<tlen;++i)
+    t[i] = t[i+1] - t[i] - overhead;
+
+  printf("%s\n", s);
+  printf("median: %d cycles/ticks\n", (int)(median(t, tlen)));
+  printf("average: %d cycles/ticks\n", (int)(average(t, tlen)));
+  printf("\n");
+}
+
+static uint32_t average_calls(int calls, size_t tlen) {
+  return calls/tlen;
+}
+
+void print_calls(const char *s, int t, size_t tlen) {
+  printf("%s\n", s);
+  printf("average: %d cycles/ticks\n", (int)(average_calls(t, tlen)));
+  printf("\n");
+}
+
+void print_cycles(const char *s, int t, size_t tlen) {
+  printf("%s \n", s);
+  printf("average: %d cycles/ticks\n", t);
+  printf("\n");
+}
+
+
